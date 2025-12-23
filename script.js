47aaa128deef9bf6cd0c74e585fbf04ef0b517a8
Bautista L. C. R. <bautycortereal@gmail.com>
2025-12-23 18:57:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
 });
+
