0fc2cefe060b84e1889c43a82f4a38794e4b15b0
Bautista L. C. R. <bautycortereal@gmail.com>
2025-12-23 18:24:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-// MENU HAMBURGUESA
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
+  navLinks.classList.toggle('show');
 });
-
-// VER MÁS ARTÍCULOS
-const verMasBtn = document.getElementById('ver-mas');
-const articulos = document.querySelectorAll('.articulo-card');
-
-let mostrados = 2;
-
-const actualizarArticulos = () => {
-    articulos.forEach((a, i) => {
-        a.style.display = i < mostrados ? 'block' : 'none';
-    });
-};
-
-verMasBtn.addEventListener('click', () => {
-    mostrados += 2;
-    actualizarArticulos();
-});
-
-actualizarArticulos();
